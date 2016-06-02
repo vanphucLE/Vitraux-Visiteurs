589dc55f989e3b4713974ad2edfe82a25668818f
Yassine Doghri <doghri.yassine@gmail.com>
2016-06-02 20:39:10 +0200

    FEATURE : Courses can be filtered.

---
+++ assets/js/vitraux-visiteurs.js
@@ -0,0 +1,16 @@
+// List.js Filtering 
+var options = {
+    valueNames: [
+        'course-title',
+        'distance',
+        'locations',
+        'duration',
+        'items'
+    ]
+};
+
+var userList = new List('courses', options);
+
+// TODO
+// Get all the courses using the Hypertopic Protocol
+// <https://github.com/Hypertopic/Protocol>
