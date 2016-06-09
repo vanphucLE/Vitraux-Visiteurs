87dca3f365c4b0bc03390ffbd571744b4a7924b1
DenisChenesseau <denis.chenesseau@utt.fr>
2016-06-09 11:22:25 +0200

    Simplification

--- test/suivreParcoursTest.rb
+++ test/suivreParcoursTest.rb
@@ -1,65 +1,24 @@
+require 'def'
+
 feature 'Suivre parcours' do
 
 	scenario 'Choisir Parcours' do
 		visit $home_page
-		isParcoursListComplete?()
-    click_on first(".parcoursButtonHomepage")
+    click_on ('Parcours "Moïse"')
     isParcoursPreviewComplete?()
-
   end
 
-  def isParcoursListComplete?()
-    has_css?(".findByKeywordHomepage")
-    has_css?(".parcoursButtonHomepage")
-    isParcoursButtonHomepageComplete?(#first(".parcoursButtonHomepage"))
-  end
-
-  def isParcoursButtonHomepageComplete?(parcoursButton)
-    parcoursButton.has_css?(".parcoursTitle")
-    parcoursButton.has_css?(".parcoursInfos")
-  end
-
-
-  def isParcoursPreviewComplete?()
-    has_css?(".titleParcoursPreview")
-    has_css?(".mapParcoursPreview")
-    has_css?(".infosParcoursPreview")
-    has_css?(".descriptionParcoursPreview")
-    has_css?(".reviewParcoursPreview")
-    has_css?(".startButtonParcoursPreview")
-  end
-
-
-
-
-
-    click_on_link 'UV'
-		click_on 'OFF'
-		click_plus_sign_next_to 'Items'
-		click_on 'Sans nom'
-		fill_in '', :with => 'IF05 - Qualité du logiciel'
-		click_on 'IF05 - Qualité du logiciel'
-		click_plus_sign_next_to 'Attributs'
-		fill_in 'Nom', :with => 'Responsable'
-		fill_in 'Valeur', :with => 'Aurélien Bénel'
-		click_on 'Créer'
-	end
-
-	scenario 'Pour un utilisateur non connecté' do
+	scenario 'Recherche Parcours' do
 		visit $home_page
-		click_on_link 'UV'
-		click_on 'Se connecter'
-		log_in_as 'alice', 'lapinblanc'
-		click_on 'Se connecter'
-		click_on 'OFF'
-		click_plus_sign_next_to 'Items'
-		click_on 'Sans nom'
-		fill_in 'Nom', :with => 'IF05 - Qualité du logiciel'
-		click_on 'IF05 - Qualité du logiciel'
-		click_plus_sign_next_to 'Attributs'
-		fill_in 'Nom', :with => 'Responsable'
-		fill_in 'Valeur', :with => 'Aurélien Bénel'
-		click_on 'Créer'
+		expect(page).to have_content 'Rechercher un thème, une époque...'
+		fill_in 'search-course', :with => 'Moïse'
+		expect(page).to have_content 'Parcours "Moïse"'
+
+		fill_in 'search-course', :with =>'15'
+		expect(page).to have_content 'Parcours "Technique Narrative"'
+
 	end
+
+
 
 end
