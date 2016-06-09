174be2971692462c5461aea518d2864f56aa0cac
DenisChenesseau <denis.chenesseau@utt.fr>
2016-06-09 20:29:55 +0200

    Les scénarions sont maintenant décomposés

--- test/suivreParcoursTest.rb
+++ test/suivreParcoursTest.rb
@@ -8,12 +8,17 @@ feature 'Suivre parcours' do
     isParcoursPreviewComplete?('Parcours "Moïse"')
   end
 
-	scenario 'Recherche Parcours' do
+	scenario 'Recherche Par Nom Du Parcours' do
 		visit $home_page
 		expect(page).to have_content 'Rechercher un thème, une époque...'
 		fill_in 'search-course', :with => 'Moïse'
 		expect(page).to have_content 'Parcours "Moïse"'
 
+	end
+	
+	scenario 'Recherche Par Nombre de Vitraux' do
+		visit $home_page
+		expect(page).to have_content 'Rechercher un thème, une époque...'
 		fill_in 'search-course', :with =>'15'
 		expect(page).to have_content 'Parcours "Technique Narrative"'
 
