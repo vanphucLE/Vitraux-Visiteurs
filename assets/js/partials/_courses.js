cb3113add25e1b2d0692f8b7a8994917f90f478e
Vincent Rajau <rajauvincent@gmail.com>
2016-12-07 01:54:06 +0100

    FEATURE: Parallel request and display on course.html

--- assets/js/partials/_courses.js
+++ assets/js/partials/_courses.js
@@ -58,122 +58,77 @@ function getCoursesList() {
             "id": "a76306e4f17ed4f79e7e481eb9a1bd06",
             "name": "Histoire de l'art"
         }
-    ];
+    ],
+        corpus = ['http://argos2.hypertopic.org/corpus/Vitraux - Bénel'];
 
 
-    function displayViewPoint(dataViewPoint){
-        //Pas besoin de retrouver ces valeurs à chaque fois
-        var viewpoint_id = dataViewPoint.rows[0].key[0],
-            viewpoint_name = dataViewPoint.rows[0].value.name;
-        $.each(dataViewPoint.rows, function(indexRow,row){
-            var topic_title = '',
-                topic_id = '',
-                items = [],
-                locations = [],
-                link = '';
+    function displayViewPoint(dataViewpoint){
+        //Nous enregistrons directement l'id et le nom du viewpoint car il s'agit à chaque fois du premier objet
+        var viewpoint_id = dataViewpoint.rows[0].key[0],
+            viewpoint_name = dataViewpoint.rows[0].value.name;
+        $.each(dataViewpoint.rows, function(indexRow,row){
             // Si le topic existe
             if(row.value.narrower){
+                 //Ajoute +1 au compteur du nombre de parcours
                  $('#courses-num').text(parseInt($('#courses-num').text()) + 1);
-                 topic_title = row.value.narrower.name;
-                 topic_id = row.value.narrower.id;
-                 link = 'map.html?viewpoint=' + viewpoint_id + '&topic=' + topic_id;
                  courseList.add({
-                        'id': topic_id,
-                        'course-title': 'Parcours "' + topic_title + '"',
+                        'id': row.value.narrower.id,
+                        'course-title': 'Parcours "' + row.value.narrower.name + '"',
                         'course-vp': "— " + viewpoint_name,
-                        'distance': '0',
-                        'locations': '0 Lieux',
-                        'duration': '0',
-                        'items-nb': '0 Vitraux'
+                        'distance': 0,
+                        'locations': 0,
+                        'locations_name' : [],
+                        'duration': 0,
+                        'items-nb': 0,
+                        'topic-link' : 'map.html?viewpoint=' + viewpoint_id + '&topic=' + row.value.narrower.id
                  });
-                 var item = courseList.get('id', topic_id)[0];
-                 item.values({'topic-link': link});    
             }
         })
-        
-
-
     }
 
+    function displayCorpus(dataCorpus){
     
-    
+      //Cette partie de la fonction permet d'afficher le nombre de vitraux pour chaque parcours
+       $.each(dataCorpus.rows,function(index,row){
+           if(row.value.topic){
+                var currentItem = courseList.get('id',row.value.topic.id)[0];
+                //Important, car il est possible que une item ne soit pas dans la liste
+                if(typeof currentItem != 'undefined'){
+                    var itemNumber = currentItem.values()['items-nb'] + 1;
+                    currentItem.values({
+                        'items-nb' : itemNumber,
+                    });
+                }
+           }
+       });
+
+      //Cette partie de la fonction permet d'afficher le nombre de lieux pour chaque parcours
+      $.each(dataCorpus.rows,function(index,row){
+         if(row.value.spatial){
+            var id_row = row.id;
+            $.each(dataCorpus.rows,function(indexVP,rowVP){
+                if(rowVP.value.topic && rowVP.id == id_row){
+                   var currentItem = courseList.get('id',rowVP.value.topic.id)[0];
+                   //Il est important de vérifier que la location n'est pas déjà présente(éviter les doublons)
+                   if(typeof currentItem != 'undefined' && currentItem.values()['locations_name'].indexOf(row.value.spatial) == -1){
+                         var locationNumber = currentItem.values()['locations'] + 1,
+                             locationNames = currentItem.values()['locations_name'].push(row.value.spatial);
+                         currentItem.values({
+                             'locations' : locationNumber
+                         })
+                   }
+
+                }
+            })
+         }
+      })
+    }  
 
       Promise.all(viewpoints.map(function(viewPointObject){
             return requestFactory('http://argos2.hypertopic.org/viewpoint/'+viewPointObject['id'],displayViewPoint);
-        }));
+        }).concat(corpus.map(function(corpusObject){
+                return requestFactory(corpusObject,displayCorpus);
+        }))
+      );
     
-
-
-
-
-    // GET Corpus Before
-    // $.ajax({
-    //     type: 'GET',
-    //     url: 'http://argos2.hypertopic.org/corpus/Vitraux - Bénel',
-    //     dataType: 'json',
-    //     success: function (corpus) {
-    //         $.each(viewpoints, function (index, vp) {
-    //             $.ajax({
-    //                 type: 'GET',
-    //                 url: 'http://argos2.hypertopic.org/viewpoint/' + vp.id,
-    //                 dataType: 'json',
-    //                 success: function (data) {
-    //                     $.each(data.rows, function (index, row) {
-    //                         var topic_title = '';
-    //                         var topic_id = '';
-    //                         var items = [];
-    //                         var locations = [];
-    //                         var link = '';
-    //                         // If topic exists (leaf)
-    //                         if (row.value.narrower) {
-    //                             $('#courses-num').text(parseInt($('#courses-num').text()) + 1);
-    //                             topic_title = row.value.narrower.name;
-    //                             topic_id = row.value.narrower.id;
-    //                             link = 'map.html?viewpoint=' + vp.id + '&topic=' + topic_id;
-    //                             courseList.add({
-    //                                 'id': topic_id,
-    //                                 'course-title': 'Parcours "' + topic_title + '"',
-    //                                 'course-vp': "— " + vp.name,
-    //                                 'distance': '0',
-    //                                 'locations': '0 Lieux',
-    //                                 'duration': '0',
-    //                                 'items-nb': '0 Vitraux'
-    //                             });
-    //                             var item = courseList.get('id', topic_id)[0];
-    //                             item.values({
-    //                                 'topic-link': link
-    //                             });
-
-    //                             $.each(corpus.rows, function (index, row) {
-    //                                 if (row.value.topic) {
-    //                                     if (row.value.topic['viewpoint'] === vp.id && row.value.topic['id'] === topic_id) {
-    //                                         items.push(row.id);
-    //                                         var item_count = parseInt(item.values()['items-nb'].replace(/^\D+/g, '')) + 1;
-    //                                         item.values({
-    //                                             'items-nb': item_count + ' ' + getItemText(item_count, "Vitrail", "Vitraux")
-    //                                         });
-    //                                     }
-    //                                 }
-    //                             });
-    //                             $.each(corpus.rows, function (index, row) {
-    //                                 if (row.value.spatial) {
-    //                                     if ($.inArray(row.id, items) > -1) {
-    //                                         locations.push(row.value.spatial);
-    //                                     }
-    //                                 }
-    //                             });
-    //                             locations = uniq(locations);
-    //                             var locations_nb = locations.length;
-    //                             var duration = getCourseDuration(locations_nb * 30);
-    //                             item.values({
-    //                                 'locations': locations_nb + ' ' + getItemText(locations_nb, "Lieu", "Lieux"),
-    //                                 'duration': duration
-    //                             });
-    //                         }
-    //                     });
-    //                 }
-    //             });
-    //         });
-    //     }
-    // });
 }
