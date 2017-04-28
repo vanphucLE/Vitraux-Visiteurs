0bc4dcd947a49fbd88186e2a5fba04456d03ec93
Maxime Dehongher <maximedehongher@gmail.com>
2017-04-28 11:54:51 +0200

    FEATURE: Capybara Evaluate my path experience

--- features/EvaluateMyPathExperience.feature
+++ features/EvaluateMyPathExperience.feature
@@ -8,8 +8,8 @@ Fonctionnalité: Faire un retour de mon expérience sur un parcours
 
   Scénario: L'utilisateur souhaite donner son avis sur son expérience de parcours.
 
-    Étant donné que Michel a effectué un parcours
-    Quand il veut donner son avis sur un parcours qu'il a effectué
-    Alors il lui serait offert la possibilité de noter son parcours
-    Et il lui serait offert la posibilité d'écrire un avis de façon libre
-    Et il lui serait offert différents labels à associer à son expérience
+    Étant donné que "Michel" a effectué un parcours
+    Quand il veut donner son avis sur le parcours qu'il a effectué
+    Alors il lui serait offert la possibilité de exprimer un avis "moyen" à son parcours
+    Et il lui serait offert la possibilité de donner un avis de façon libre et d'écrire "Le parcours que je me suis construit n'était pas adapté pour mes enfants. Michel"
+    Et il lui serait offert la possibilité de mentionner que son parcours a été de type "familiale" en le labélisant
---
+++ features/step_definitions/VisitClosePlace.rb
@@ -0,0 +1,27 @@
+Étantdonnéque(/^le touriste se situe à une certaine position$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quand(/^il veut connaître l'endroît le plus proche contenant des vitraux$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Alors(/^il lui serait proposer la localisation du lieu le plus proche de lui$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Alors(/^il lui serait fournit le chemin le plus court pour y accéder$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Alors(/^il lui serait fournit la distance et le temps de trajet$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quand(/^il veut connaître l'endroît le plus proche contenant des vitraux qui l'intéressent$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Alors(/^il lui serait demander de mentioner ses attentes en matière de vitraux$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
